f3d545d7cad6ff089203e261e61918c6ca28d88a
Myllena <WELLERSON@192.168.25.8>
2016-02-02 19:54:17 -0300

    Alterações na activity de valor calórico, recuperando radioButtons

--- NutrIF/src/br/edu/ifpb/nutrif/activity/CalculaVCTActivity.java
+++ NutrIF/src/br/edu/ifpb/nutrif/activity/CalculaVCTActivity.java
@@ -8,6 +8,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import br.edu.ifpb.nutrif.R;
 import br.edu.ifpb.nutrif.asynctask.*;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class CalculaVCTActivity extends Activity {
 
@@ -26,23 +28,54 @@ public class CalculaVCTActivity extends Activity {
 	            String peso = pesoEditText.getText().toString();
 	            EditText alturaEditText = (EditText) findViewById(R.id.alturaEditText);
 	            String altura = alturaEditText.getText().toString();
-	            EditText esporteEditText = (EditText) findViewById(R.id.niveis);
-	            String esporte = esporteEditText.getText().toString();
-	            EditText sexoEditText = (EditText) findViewById(R.id.sexo);
-	            String sexo = sexoEditText.getText().toString();
 	            EditText nascimentoEditText = (EditText) findViewById(R.id.nasc);
 	            String nascimento = nascimentoEditText.getText().toString();
 	            
+	            //recuperando variaveis do radio button de niveis de esporte
+	            
+	            String esporte="";
+	            
+	            RadioGroup rg = (RadioGroup)findViewById(R.id.rgopcoes);
+	            int op = rg.getCheckedRadioButtonId();
+	             
+	            if(op==R.id.radioButton1){
+	            	RadioButton NivelEsporte = (RadioButton) findViewById(R.id.radioButton1);
+	            	esporte = NivelEsporte.getText().toString();
+	            }           
+	            else
+	            if(op==R.id.radioButton2){
+	            	RadioButton NivelEsporte = (RadioButton) findViewById(R.id.radioButton1);
+	            	esporte = NivelEsporte.getText().toString();
+	            }
+	            else
+	            	if (op==R.id.radioButton3){
+	            		RadioButton NivelEsporte = (RadioButton) findViewById(R.id.radioButton1);
+	            		esporte = NivelEsporte.getText().toString();}
+	            
+	            //recuperando variaveis do radio button de niveis de sexo
+	            
+	            String sexo="";
+	                         
+	            if(op==R.id.radioButton4){
+	            	RadioButton sexoRadio = (RadioButton) findViewById(R.id.radioButton4);
+	            	sexo = sexoRadio.getText().toString();
+	            }           
+	            else
+	            if(op==R.id.radioButton5){
+	            	RadioButton sexoRadio = (RadioButton) findViewById(R.id.radioButton5);
+	            	sexo = sexoRadio.getText().toString();
+	            }
+	            
 
-	            String [] valores = {peso, altura, esporte, sexo, nascimento};
+	           String [] valores = {peso, altura, esporte, sexo, nascimento};
 	     	            
 	            CalButtonAsyncTask calButtonAsynctask = new CalButtonAsyncTask(v.getContext());
 	            
-	            calButtonAsynctask.execute(valores);
+	           calButtonAsynctask.execute(valores);
 	            
 	        }
 	    });
 		
 
 	}
-	}
+}
